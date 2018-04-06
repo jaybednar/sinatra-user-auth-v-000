2b7b79961bf06f2a74e3234322f328c18c618793
Jay Bednar <ironviralmedia@gmail.com>
2018-04-06 20:28:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
       session[:id] = @user.id
       redirect '/users/home'
     else
-      erb :'sessions/login'
+      redirect '/sessions/login'
     end
   end
 
